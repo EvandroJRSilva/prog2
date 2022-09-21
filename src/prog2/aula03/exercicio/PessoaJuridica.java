d2983d9b1ec204896efcba5d0f791871bf51e138
Evandro J.R. Silva <ejrs.profissional@gmail.com>
2022-09-21 20:08:12 -0300

    atualizado

--- src/prog2/aula03/exercicio/PessoaJuridica.java
+++
@@ -1,9 +0,0 @@
-package prog2.aula03.exercicio;
-
-public interface PessoaJuridica 
-{
-	public String getCNPJ();
-	public void setCNPJ(String cnpj);
-	public String getNomeFantasia();
-	public void setNomeFantasia(String nomeFantasia);
-}
